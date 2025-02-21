3f83c764f7d3aef49cdc8a0df9f7494afbedd752
Matthew Clonch <matthewrclonch@gmail.com>
2025-02-21 13:13:23 -0600

    wip

---
+++ client/src/ThemeContext/ThemeContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      darkMode ? "dark" : "light"
+    );
+  }, [darkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
