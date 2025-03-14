7cf48dabf765cc7cfc8a233473273d60561c9b0c
Matthew Clonch <matthewrclonch@gmail.com>
2025-03-14 11:45:14 -0500

    add item to character functionality

--- client/src/managers/itemManager.js
+++ client/src/managers/itemManager.js
@@ -28,3 +28,27 @@ export const getAllItems = async () => {
 
   return response.json();
 };
+
+//Adds an item to a character by creating a new characterItem entity
+//Expects the id of the item being added, the id of the character the item is being added to, and the quantity
+export const newCharacterItem = async (itemId, characterId, quantity) => {
+  const response = await fetch(
+    `${_apiString}/characterAdd/${itemId}?characterId=${characterId}&quantity=${quantity}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response === 404) {
+    return response.text();
+  }
+
+  if (!response.ok) {
+    throw new Error(`HTTP Error! Status: ${response.status}`);
+  }
+
+  return response.json();
+};
